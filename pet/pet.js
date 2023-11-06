5cc39beeb1b6bf35d937d2d8b43210f0250c7292
Asura0107 <mircoorosio@gmail.com>
2023-11-06 16:54:28 +0100

    U2-W2-D1-JSIII

---
+++ pet/pet.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector("button");
+const petname = document.getElementById("petname");
+const ownername = document.getElementById("ownername");
+const species = document.getElementById("species");
+const breed = document.getElementById("breed");
+const listul = document.getElementById("list");
+
+btn.onclick = function () {
+  //   e.preventDefault();
+
+  const text = ownername.value;
+  console.log(text);
+  const text1 = petname.value;
+  console.log(text1);
+  const text2 = species.value;
+  console.log(text2);
+  const text3 = breed.value;
+  console.log(text3);
+
+  const sign = text + " " + text1 + " " + text2 + " " + text3;
+  console.log(sign);
+  const element = document.createElement("li");
+  const span = document.createElement("span");
+
+  span.innerText = sign;
+  element.appendChild(span);
+  listul.appendChild(element);
+  ownername.value = "";
+  petname.value = "";
+  species.value = "";
+  breed.value = "";
+};

70bb955fe6e76efe3b029135d14a637eceded944
Asura0107 <mircoorosio@gmail.com>
2023-11-06 19:34:44 +0100

    U2-W2-D1-JSIII

--- pet/pet.js
+++ pet/pet.js
@@ -6,26 +6,41 @@ const breed = document.getElementById("breed");
 const listul = document.getElementById("list");
 const form = document.getElementById("pet");
 
+let sign = [];
+// mi creo un array iniziale che mi andrà a contenere il mio object
+// mi creo le variabile che mi servono
+class Pet {
+  constructor(petname, ownername, species, breed) {
+    this.petname = petname;
+    this.ownername = ownername;
+    this.species = species;
+    this.breed = breed;
+  }
+}
+
+// all'intern della funzione foreach mi prendo un parametro che mi va a rappresentare ogni pt che l'utente iserirà
+// per ogi  parametro mi creo un li con un suo innertext composto dai parametri della classe precedente
+const assign = function () {
+  sign.forEach((pet) => {
+    const newli = document.createElement("li");
+    newli.innerText =
+      "nome: " + pet.petname + " , proprietario: " + pet.ownername;
+    listul.appendChild(newli);
+  });
+};
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  //   mi creo il mio nuovo oggetto e lo metto all'interno dell'array vuoto, infine richiamo la funzione di prima che mi va a creare i miei li
+  const newpet = new Pet(
+    ownername.value,
+    petname.value,
+    species.value,
+    breed.value
+  );
+  sign.push(newpet);
+  assign();
 
-  const text = ownername.value;
-  console.log(text);
-  const text1 = petname.value;
-  console.log(text1);
-  const text2 = species.value;
-  console.log(text2);
-  const text3 = breed.value;
-  console.log(text3);
-
-  const sign = text + " " + text1 + " " + text2 + " " + text3;
-  console.log(sign);
-  const element = document.createElement("li");
-  const span = document.createElement("span");
-
-  span.innerText = sign;
-  element.appendChild(span);
-  listul.appendChild(element);
   ownername.value = "";
   petname.value = "";
   species.value = "";
